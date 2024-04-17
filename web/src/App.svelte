5490cb11a768a7cdec718641618ca972cb16752a
myferonguilded <michaelthecrumbyt1@gmail.com>
2024-04-16 22:16:21 -0500

    Create

---
+++ web/src/App.svelte
@@ -0,0 +1,19 @@
+<script>
+  import sparklesSVG from './assets/sparkle.svg';
+</script>
+
+<main>
+  <div id="bar">
+    <b>DevKinect</b><a href="https://github.com/DevKinect">GitHub</a><a href="mailto:devkinect@proton.me">Email us</a><a href="products.html">Products</a><a href="#">Discord</a>
+  </div>
+  <div id="main">
+    <h1>Code. Faster. <img src=" {sparklesSVG} " alt=""></h1>
+    <p>DevKinect provides tools for developers to use around the<br>globe, from CLIs to even code editors.</p>
+
+    <br><a href="https://github.com/DevKinect" target="_blank">
+      <button>
+        Our GitHub
+      </button>
+    </a>
+  </div>
+</main>
